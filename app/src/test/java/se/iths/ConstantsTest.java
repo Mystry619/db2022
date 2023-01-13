064ec2d8ee0c45a97f19b46895921d8c62252581
Bashar <bashar.egho@iths.com>
2023-01-13 18:27:51 +0100

    create CRUD program på test grenen

---
+++ app/src/test/java/se/iths/ConstantsTest.java
@@ -0,0 +1,12 @@
+package se.iths;
+
+public class ConstantsTest {
+    public static final String JDBC_CONNECTION = "jdbc:mysql://localhost:3306/iths";
+    public static final String JDBC_USER = "iths";
+    public static final String JDBC_PASSWORD = "iths";
+    public static final String TEST_EMPLOY = "Andrew Adams";
+    public static final String TEST_SECOND_EMPLOY = "Steve Johnson";
+    public static final String TEST_JOB = "Programmer";
+    public static final String TEST_NEW_JOB = "Project manager";
+
+}
